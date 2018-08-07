00ee9556d979056b0a458fd69fa23dd6ac48bc9c
horis233 <horis233@gmail.com>
2018-08-07 16:58:37 -0400

    add backdrop

--- lib/category.dart
+++ lib/category.dart
@@ -1,115 +1,27 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// To keep your imports tidy, follow the ordering guidelines at
-// https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-import 'package:flutter_app/unit.dart';
-import 'package:flutter_app/converter_route.dart';
 
-/// A custom [Category] widget.
-///
-/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
-/// a colored [InkWell] animation.
-///
+import 'unit.dart';
 
-final _rowHeight = 100.0;
-final _borderRadius = BorderRadius.circular(_rowHeight / 2);
-
-class Category extends StatelessWidget {
-  /// Creates a [Category].
-  ///
-  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
-  /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
+/// A [Category] keeps track of a list of [Unit]s.
+class Category {
   final String name;
   final ColorSwatch color;
-  final IconData iconLocation;
   final List<Unit> units;
+  final IconData iconLocation;
 
-
+  /// Information about a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), a list of its
+  /// its color for the UI, units for conversions (e.g. 'Millimeter', 'Meter'),
+  /// and the icon that represents it (e.g. a ruler).
   const Category({
-    Key key,
     @required this.name,
     @required this.color,
-    @required this.iconLocation,
     @required this.units,
-}) : assert(name != null),
-     assert(units != null),
-     assert(color != null),
-     assert(iconLocation != null),
-     super(key:key);
-
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  ///
-  void _navigateToConverter(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute<Null>(
-      builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 1.0,
-            title: Text(
-              name,
-              style: Theme.of(context).textTheme.display1,
-            ),
-            centerTitle: true,
-            backgroundColor: color,
-          ),
-          body: ConverterRoute(
-            color: color,
-            name: name,
-            units: units,
-          ),
-        );
-      },
-    ));
-  }
-
-  /// This information includes the icon, name, and color for the [Category].
-  @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: _borderRadius,
-          highlightColor: color,
-          splashColor: color,
-          // We can use either the () => function() or the () { function(); }
-          // syntax.
-          onTap: () => _navigateToConverter(context),
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 60.0,
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null),
+        assert(iconLocation != null);
 }
